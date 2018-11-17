8bd1d45ba244bb22e4f0b1e0133cecee6ffdad44
Derik Linch <dlinch33@gmail.com>
2018-11-17 16:47:24 -0700

    section 1

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,41 @@
 defmodule Cards do
   @moduledoc """
-  Documentation for Cards.
+  An elixir module to run common functions on a deck of cards.
   """
 
-  @doc """
-  Hello world.
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-  ## Examples
+    # Not so good
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
 
-      iex> Cards.hello()
-      :world
+    # List.flatten(cards)
 
-  """
+    # Better
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
   def hello do
-    :world
+    "Hi there!"
+    # :world
   end
 end
